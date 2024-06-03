215cc8b547f26a950de7975dc735163ce2d5c777
Leonardo Alipazaga <leoalipazaga@Leonardos-MBP-2.bbrouter>
2024-06-02 23:20:29 -0500

    feat: add nvim settings

---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  -- "nvim-treesitter/nvim-treesitter",
+  -- build = ":TSUpdate",
+  -- config = function()
+  --   local treesitter = require("nvim-treesitter.configs")
+  --   treesitter.setup({
+  --     highlight = { enable = true },
+  --     indent = { enable = true },
+  --     ensure_installed = {
+  --       "svelte",
+  --     },
+  --   })
+  -- end,
+}
