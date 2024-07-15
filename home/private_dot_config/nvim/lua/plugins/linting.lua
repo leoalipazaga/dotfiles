45970d3ac62e694b5873d5b190290a3476d33982
Leonardo Alipazaga <leoalipazaga@Leonardos-MBP-2.bbrouter>
2024-07-15 12:40:09 -0500

    feat: add .chezmoiroot file

---
+++ home/private_dot_config/nvim/lua/plugins/linting.lua
@@ -0,0 +1,38 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+    local eslint = lint.linters.eslint_d
+    -- HACK: https://github.com/mfussenegger/nvim-lint/issues/462#issuecomment-1986702915
+    eslint.args = {
+      "--no-warn-ignored", -- <-- this is the key argument
+      "--format",
+      "json",
+      "--stdin",
+      "--stdin-filename",
+      function()
+        return vim.api.nvim_buf_get_name(0)
+      end,
+    }
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      astro = { "eslint_d" },
+      svelte = { "eslint_d" },
+      -- go = { "golangci-lint" },
+    }
+
+    local lint_group = vim.api.nvim_create_augroup("lint", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      -- pattern = { "*.ts", "*.js" },
+      group = lint_group,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
---
+++ home/private_dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,50 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- "jay-babu/mason-null-ls.nvim"
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
+    -- local mason_null_ls = require("mason-null-ls")
+    mason.setup({})
+    mason_lspconfig.setup({
+      ensure_installed = {
+        -- typescript
+        "tsserver",
+        "css-lsp",
+        -- "cssmodule_ls",
+        "emmet_ls",
+        "astro",
+        "graphql",
+        "tailwindcss",
+        -- yaml
+        "hydra_lsp",
+        -- toml
+        "taplo",
+        "svelte",
+        "sqls",
+        "html",
+        "htmx",
+        "prismals",
+        -- json
+        "biome",
+        "lua",
+        "gopls",
+        "eslint",
+      },
+      automatic_installation = true,
+    })
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prismals",
+        "prettier",
+        "stylua",
+        "eslint_d",
+      },
+    })
+  end,
+}
