1b709e21261d57a232692cd17895e9d3ef330403
Leonardo Alipazaga <leoalipazaga@Leonardos-MBP-2.bbrouter>
2024-07-28 11:38:10 -0500

    feat: find files by omiting npm packages, vscode, .git & .vscode folders

--- home/private_dot_config/nvim/lua/plugins/telescope.lua
+++ home/private_dot_config/nvim/lua/plugins/telescope.lua
@@ -3,4 +3,52 @@ return {
   tag = "0.1.6",
   -- or                              , branch = '0.1.x',
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "<leader>fh",
+      desc = "Find files (omit NPM packages)",
+      function()
+        require("telescope.builtin").find_files({
+          hidden = true,
+          find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            "--glob=!.git/**",
+            "--glob=!node_modules/**",
+            "--glob=!.vscode/",
+            "--glob=!temp/",
+            "--glob=!.next/**",
+          },
+        })
+      end,
+      { noremap = true, silent = true },
+    },
+    {
+      "<leader>s.",
+      desc = "Grep root dir (omit NPM packages)",
+      function()
+        require("telescope.builtin").live_grep({
+          additional_args = function()
+            return { "--hidden" }
+          end,
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--glob=!.git/**",
+            "--glob=!node_modules/**",
+            "--glob=!tmp/**",
+            "--glob=!.vscode/",
+            "--glob=!.next/**",
+          },
+        })
+      end,
+      { noremap = true, silent = true },
+    },
+  },
 }
