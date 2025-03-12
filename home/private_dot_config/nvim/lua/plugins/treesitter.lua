2c93b9b81bf7057c99313671301ad5bff79c0936
Leonardo Alipazaga <leonardoalipazagab@gmail.com>
2025-03-12 15:10:01 -0500

    feat: remove build prop

--- home/private_dot_config/nvim/lua/plugins/treesitter.lua
+++ home/private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,32 @@
 -- below configuration is overrite <C-leader> keymap
 return {
-  "nvim-treesitter/nvim-treesitter",
-  cmd = {
-    "TSInstall",
-    "TSBufEnable",
-    "TSBufDisable",
-    "TSModuleInfo",
-    "TSInstallFromGrammar",
-  },
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "astro",
-      "svelte",
-      "css",
-      "html",
-      "javascript",
-      "tsx",
-      "typescript",
-      "yaml",
-      "lua",
-      "luadoc",
-      "vim",
-      "vimdoc",
-      "markdown",
-      "markdown_inline",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    cmd = {
+      "TSInstall",
+      "TSBufEnable",
+      "TSBufDisable",
+      "TSModuleInfo",
+      "TSInstallFromGrammar",
+    },
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "svelte",
+        "css",
+        "html",
+        "javascript",
+        "tsx",
+        "typescript",
+        "yaml",
+        "lua",
+        "luadoc",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+      },
     },
   },
 }
